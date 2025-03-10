288b903241a65042605fcfb6fcb13c6f31e3c437
yvonne8263 <vivian826826@gmail.com>
2025-03-10 14:22:38 +0800

    Add files via upload

---
+++ Project_restaurant/js/script.js
@@ -0,0 +1,21 @@
+let menu=document.querySelector("#menu-bar")
+let navbar=document.querySelector(".navbar")
+
+// 按下按鈕產生的變化
+
+menu.onclick=() =>{
+    menu.classList.toggle('fa-times')
+    navbar.classList.toggle('active')
+}
+
+//回到上層按鈕
+
+window.onscroll = () =>{
+
+    if(window.scrollY>60){
+        document.querySelector('#scroll-up').classList.add('active');
+
+    }else {
+        document.querySelector('#scroll-up').classList.remove('active');
+    }
+}
